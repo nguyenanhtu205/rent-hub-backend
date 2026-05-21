7e0982510a181b430953e06b9b2cb477805ac15c
nguyenanhtu <nguyenanhtu16042k5@gmail.com>
2026-05-21 11:21:16 +0700

    feat(db): add ActiveWorkCount to Staff

---
+++ src/Infrastructure/Migrations/20260521012121_AddActiveWorkCountToStaff.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActiveWorkCountToStaff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "active_work_count",
+                table: "staff",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "active_work_count",
+                table: "staff");
+        }
+    }
+}
