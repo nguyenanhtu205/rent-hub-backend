6605700fa24873599a78f41ee3ca2549c665816c
nguyenanhtu <nguyenanhtu16042k5@gmail.com>
2026-05-30 21:48:58 +0700

    feat: add GetCustomers endpoint

---
+++ src/Application/Features/Customers/Queries/GetCustomer.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Features.Customers.Queries;
+
+public record CustomerDto(string Name, string Phone, CustomerType Type);
+
+public record GetCustomerQuery : IRequest<List<CustomerDto>>;
+
+public class GetCustomerQueryHandler(IApplicationDbContext context)
+    : IRequestHandler<GetCustomerQuery, List<CustomerDto>>
+{
+    public async Task<List<CustomerDto>> Handle(GetCustomerQuery request, CancellationToken cancellationToken)
+    {
+        return await context.Customers
+            .Select(c => new CustomerDto(c.Name, c.Phone, c.Type))
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ src/Web/Endpoints/Customers.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Customers.Queries;
+
+namespace Web.Endpoints;
+
+public class Customers : IEndpointGroup
+{
+    public static void Map(RouteGroupBuilder groupBuilder)
+    {
+        groupBuilder.MapGet("/", GetCustomers)
+            .RequireAuthorization("Manager")
+            .Produces<List<CustomerDto>>()
+            .RequireRateLimiting("get");
+    }
+
+    [EndpointSummary("Get all customers")]
+    public static async Task<IResult> GetCustomers(ISender sender, CancellationToken cancellationToken)
+    {
+        List<CustomerDto> customers = await sender.Send(new GetCustomerQuery(), cancellationToken);
+        return Results.Ok(customers);
+    }
+}
