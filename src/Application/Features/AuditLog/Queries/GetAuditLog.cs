b09c306d78bb53efce44788a9269e611f187884c
nguyenanhtu <nguyenanhtu16042k5@gmail.com>
2026-05-30 21:06:51 +0700

    feat: add GetAuditLogs endpoint

---
+++ src/Application/Features/AuditLog/Queries/GetAuditLog.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Features.AuditLog.Queries;
+
+public record AuditLogDto(string RequestName, string? UserId, DateTimeOffset CreatedAt);
+
+public record GetAuditLogQuery : IRequest<List<AuditLogDto>>;
+
+public class GetAuditLogQueryHandler(IApplicationDbContext context)
+    : IRequestHandler<GetAuditLogQuery, List<AuditLogDto>>
+{
+    public async Task<List<AuditLogDto>> Handle(GetAuditLogQuery request, CancellationToken cancellationToken)
+    {
+        return await context.AuditLogs
+            .OrderByDescending(x => x.CreatedAt)
+            .Select(x => new AuditLogDto(
+                x.RequestName,
+                x.UserId,
+                x.CreatedAt))
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ src/Web/Endpoints/AuditLogs.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.AuditLog.Queries;
+
+namespace Web.Endpoints;
+
+public class AuditLogs : IEndpointGroup
+{
+    public static void Map(RouteGroupBuilder groupBuilder)
+    {
+        groupBuilder.MapGet(GetAuditLogs)
+            .RequireAuthorization("Manager")
+            .Produces<List<AuditLogDto>>()
+            .RequireRateLimiting("get");
+    }
+
+    [EndpointSummary("Get audit logs")]
+    public static async Task<IResult> GetAuditLogs(ISender sender, CancellationToken cancellationToken)
+    {
+        List<AuditLogDto> auditLogs = await sender.Send(new GetAuditLogQuery(), cancellationToken);
+        return Results.Ok(auditLogs);
+    }
+}
